819011e018df25fb9b619a3a4aed573a41f574f1
mila <kamilia.latifah@gmail.com>
2019-11-29 21:57:50 +0700

    import firebase

--- chattapp-Kamilia Latifah/SceneDelegate.swift
+++ chattapp-Kamilia Latifah/SceneDelegate.swift
@@ -14,12 +14,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.setNavigation()
+        self.window = UIWindow(windowScene: windowScene)
+        var rootView: UIViewController = LoginPage(nibName: "LoginPage", bundle: nil) 
+        self.window?.rootViewController = UINavigationController(rootViewController: rootView)
+        self.window?.makeKeyAndVisible()
     }
-
+    
+    func setNavigation() {
+        let navigationBarAppearance = UINavigationBar.appearance()
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            navigationBarAppearance.standardAppearance = appearance
+            navigationBarAppearance.compactAppearance = appearance
+            navigationBarAppearance.scrollEdgeAppearance = appearance
+        } else {
+            navigationBarAppearance.isTranslucent = false
+        }
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.

24b1b330fa9ad1f2e63176c82d0cf2796bef98a4
mila <kamilia.latifah@gmail.com>
2019-11-29 17:24:14 +0700

    Initial Commit

---
+++ chattapp-Kamilia Latifah/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chattapp-Kamilia Latifah
+//
+//  Created by Kamilia Latifah on 29/11/19.
+//  Copyright © 2019 masaksendiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
